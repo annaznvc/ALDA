e4db966e4aceb09d4b0b1cc317e4de1afdee9c8b
Anna Zynovchenko <annazynovchenko05@gmail.com>
2025-04-10 12:28:28 +0200

    BinaryDictionary angepasst

--- dictionary/dictionary/BinaryTreeDictionary.java
+++ dictionary/dictionary/BinaryTreeDictionary.java
@@ -28,7 +28,7 @@ public class BinaryTreeDictionary<K extends Comparable<K>, V> implements Diction
     private Node<K, V> root = null;
     private int size = 0;
 
-    /////////////////////////////////////
+    //übernommen aus 3-12
     @Override
     public V insert(K key, V value) {
         V[] old = (V[]) new Object[1];
@@ -36,8 +36,7 @@ public class BinaryTreeDictionary<K extends Comparable<K>, V> implements Diction
         return old[0];
     }
 
-
-
+    // Übernommen aus 4-18
     private Node<K, V> insertR(K key, V value, Node<K, V> t, Node<K, V> parent, V[] old) {
         if (t == null) {
             size++;
@@ -57,53 +56,73 @@ public class BinaryTreeDictionary<K extends Comparable<K>, V> implements Diction
         return balance(t);
     }
 
+    // Übernommen aus 3-10
     @Override
     public V search(K key) {
-        Node<K, V> t = root;
-        while (t != null) {
-            int cmp = key.compareTo(t.key);
-            if (cmp < 0)
-                t = t.left;
-            else if (cmp > 0)
-                t = t.right;
-            else
-                return t.value;
-        }
-        return null;
+        return searchR(key, root);
     }
+
+    private V searchR(K key, Node<K, V> p) {
+        if (p == null)
+            return null;
+        else if (key.compareTo(p.key) < 0)
+            return searchR(key, p.left);
+        else if (key.compareTo(p.key) > 0)
+            return searchR(key, p.right);
+        else
+            return p.value;
+    }
+
+    //übernommen aus 3-16
+    private V oldValue; // Rückgabeparameter für remove
 
     @Override
     public V remove(K key) {
-        V[] old = (V[]) new Object[1];
-        root = removeR(key, root, old);
-        return old[0];
+        root = removeR(key, root);
+        return oldValue;
     }
 
-    private Node<K, V> removeR(K key, Node<K, V> t, V[] old) {
-        if (t == null)
-            return null;
-        int cmp = key.compareTo(t.key);
-        if (cmp < 0)
-            t.left = removeR(key, t.left, old);
-        else if (cmp > 0)
-            t.right = removeR(key, t.right, old);
-        else {
-            old[0] = t.value;
+    private Node<K, V> removeR(K key, Node<K, V> p) {
+        if (p == null) {
+            oldValue = null;
+        } else if (key.compareTo(p.key) < 0) {
+            p.left = removeR(key, p.left);
+        } else if (key.compareTo(p.key) > 0) {
+            p.right = removeR(key, p.right);
+        } else if (p.left == null || p.right == null) {
+            // p muss gelöscht werden (0 oder 1 Kind)
+            oldValue = p.value;
             size--;
-            if (t.left == null)
-                return t.right;
-            else if (t.right == null)
-                return t.left;
-            else {
-                Node<K, V> u = t.right;
-                while (u.left != null)
-                    u = u.left;
-                t.key = u.key;
-                t.value = u.value;
-                t.right = removeR(u.key, t.right, (V[]) new Object[1]);
-            }
+            p = (p.left != null) ? p.left : p.right;
+        } else {
+            // p hat zwei Kinder
+            MinEntry<K, V> min = new MinEntry<>();
+            p.right = getRemMinR(p.right, min);
+            oldValue = p.value;
+            p.key = min.key;
+            p.value = min.value;
+            size--;
         }
-        return balance(t);
+        return (p != null) ? balance(p) : null;
+    }
+
+    //löscht im Baum p den Knoten mit kleinstem Schlüssel und liefert dessen Daten zurück
+    private Node<K, V> getRemMinR(Node<K, V> p, MinEntry<K, V> min) {
+        assert p != null;
+        if (p.left == null) {
+            min.key = p.key;
+            min.value = p.value;
+            return p.right;
+        } else {
+            p.left = getRemMinR(p.left, min);
+            return balance(p); // AVL-Balancierung beim Rückweg
+        }
+    }
+
+    //Hildsdatentyp für Rücgabeparameter min von getRemMinR
+    private static class MinEntry<K, V> {
+        private K key;
+        private V value;
     }
 
     @Override
@@ -111,83 +130,91 @@ public class BinaryTreeDictionary<K extends Comparable<K>, V> implements Diction
         return size;
     }
 
+    
     @Override
     public Iterator<Entry<K, V>> iterator() {
         return new Iterator<>() {
-            Stack<Node<K, V>> stack = new Stack<>();
+            Stack<Node<K, V>> stack = new Stack<>(); //noch zu bearbeitenden Knoten speichern
             Node<K, V> current = root;
 
             @Override
             public boolean hasNext() {
-                return !stack.isEmpty() || current != null;
+                return !stack.isEmpty() || current != null; //stack nicht leer oder current nicht null (wir haben noch einen Teilbaum zu betreten)
             }
 
             @Override
             public Entry<K, V> next() {
-                while (current != null) {
+                while (current != null) { //solange wir weiter nach links können, gehen wir links
                     stack.push(current);
-                    current = current.left;
+                    current = current.left; //wir merken uns die knoten
                 }
-                Node<K, V> node = stack.pop();
-                current = node.right;
-                return new Entry<>(node.key, node.value);
+                Node<K, V> node = stack.pop(); //wenn wir am linken ende angekommen sind, holen wir letzten gespeichert Knoten aus dem Stack
+                current = node.right; //setzen current auf seinen rechten teimbaum (in order: links, wurzel, rechts)
+                return new Entry<>(node.key, node.value); //aktuellen Knoten als Entry zurückliefern
             }
         };
     }
 
-    private Node<K, V> balance(Node<K, V> t) {
-        updateHeight(t);
-        int bf = getBalance(t);
-        if (bf == 2) {
-            if (getBalance(t.right) < 0)
-                t.right = rotateRight(t.right);
-            return rotateLeft(t);
-        } else if (bf == -2) {
-            if (getBalance(t.left) > 0)
-                t.left = rotateLeft(t.left);
-            return rotateRight(t);
+    //Übernommen aus 4-18
+    private Node<K, V> balance(Node<K, V> p) {
+        if (p == null)
+            return null;
+        p.height = Math.max(getHeight(p.left), getHeight(p.right)) + 1;
+        if (getBalance(p) == -2) {
+            if (getBalance(p.left) <= 0)
+                p = rotateRight(p);
+            else
+                p = rotateLeftRight(p);
+        } else if (getBalance(p) == +2) {
+            if (getBalance(p.right) >= 0)
+                p = rotateLeft(p);
+            else
+                p = rotateRightLeft(p);
         }
-        return t;
+        return p;
     }
 
-    private void updateHeight(Node<K, V> t) {
-        t.height = 1 + Math.max(height(t.left), height(t.right));
+    private int getHeight(Node<K, V> p) {
+        return (p == null) ? -1 : p.height;
     }
 
-    private int height(Node<K, V> t) {
-        return t == null ? -1 : t.height;
+    private int getBalance(Node<K, V> p) {
+        return getHeight(p.right) - getHeight(p.left);
     }
 
-    private int getBalance(Node<K, V> t) {
-        return height(t.right) - height(t.left);
+    //übernommen aus 4-20
+    private Node<K, V> rotateRight(Node<K, V> p) {
+        assert p.left != null;
+        Node<K, V> q = p.left;
+        p.left = q.right;
+        q.right = p;
+        p.height = Math.max(getHeight(p.left), getHeight(p.right)) + 1;
+        q.height = Math.max(getHeight(q.left), getHeight(q.right)) + 1;
+        return q;
     }
 
-    private Node<K, V> rotateLeft(Node<K, V> t) {
-        Node<K, V> u = t.right;
-        t.right = u.left;
-        if (u.left != null) u.left.parent = t;
-        u.left = t;
-        u.parent = t.parent;
-        t.parent = u;
-        updateHeight(t);
-        updateHeight(u);
-        return u;
+    //übernommen aus 4-21
+    private Node<K, V> rotateLeft(Node<K, V> p) {
+        assert p.right != null;
+        Node<K, V> q = p.right;
+        p.right = q.left;
+        q.left = p;
+        p.height = Math.max(getHeight(p.left), getHeight(p.right)) + 1;
+        q.height = Math.max(getHeight(q.left), getHeight(q.right)) + 1;
+        return q;
+    }
+    private Node<K, V> rotateLeftRight(Node<K, V> p) {
+        assert p.left != null;
+        p.left = rotateLeft(p.left);
+        return rotateRight(p);
+    }
+    private Node<K, V> rotateRightLeft(Node<K, V> p) {
+        assert p.right != null;
+        p.right = rotateRight(p.right);
+        return rotateLeft(p);
     }
 
-    private Node<K, V> rotateRight(Node<K, V> t) {
-        Node<K, V> u = t.left;
-        t.left = u.right;
-        if (u.right != null) u.right.parent = t;
-        u.right = t;
-        u.parent = t.parent;
-        t.parent = u;
-        updateHeight(t);
-        updateHeight(u);
-        return u;
-    }
-
-
-    //////////////////////////////////7
+    //vorgegeben:
     public void prettyPrint() {
         printR(0, root);
     }
